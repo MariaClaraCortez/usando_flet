02fb0d5ead498903268c54da9ad70cea6a7dedf5
Maria Clara Cortez <maria.c.dias@edu.senai.br>
2026-08-04 14:50:05 -0300

    ADD testando flet

---
+++ hello_world_flet.py
@@ -0,0 +1,57 @@
+import flet as ft
+
+def main(pagina:ft.Page):
+    #ALTERANDO O TITULO DA JANLA 
+    pagina.title = "Super Programa de Hello World!!"
+    #ALTERANDO A COR DA JANELA
+    pagina.bgcolor="#EAF3FA"  
+    #ALTERANDO A ALTURA DA JANELA
+    pagina.window.height = 650
+    #ALTERANDO A LARGURA DA JANELA
+    pagina.window.width = 1200
+    #CENTRALIZANDO O TEXTO
+    pagina.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    #CRIANDO UM TEXTO
+    texto_hello =ft.Text(value="Olá Pessoal!!",
+                        color ="#A0C6E9",
+                        font_family="italic",
+                        size=50,
+                        weight=ft.FontWeight.BOLD,)
+                        
+    subtitulo =ft.Text(value="Sejam Bem Vindos a Minha Fliricultira!",
+                       color ="#A0C6E9",
+                       font_family="consolas",
+                       size=30,
+                       weight=ft.FontWeight.BOLD,)
+    #FUNÇAO PARA O BOTAO
+    def mostar_imagem():
+        if imagem.visible == True:
+            imagem.visible =False 
+        else:
+            imagem.visible =True
+
+           
+    #CRIANDO BOTAO 
+    botao =ft.Button(content="Clique Aqui",
+                     color="#C5DCF1",
+                     on_click=mostar_imagem)
+    #CRIANDO UMA IMAGEM
+    imagem =ft.Image(src="img/tulipa.jpg",
+                    width=150,
+                    height=150,
+                    border_radius =20,
+                    visible=False)
+    #ADICIONANDO O TEXTO PA PAGINA
+    pagina.add(texto_hello)
+    pagina.add(subtitulo)
+    pagina.add(botao)
+    pagina.add(imagem)
+    pagina.update()
+
+
+
+
+
+
+
+ft.run(main)
---
+++ mensagens.py
@@ -0,0 +1,108 @@
+import flet as ft
+import random
+
+def main(pagina:ft.Page):
+    #ALTERANDO O TITULO DA JANLA 
+    pagina.title = "Super Programa de Mensagens!!"
+    #ALTERANDO A COR DA JANELA
+    pagina.bgcolor="#EAF3FA"  
+    #ALTERANDO A ALTURA DA JANELA
+    pagina.window.height = 600
+    #ALTERANDO A LARGURA DA JANELA
+    pagina.window.width = 1000
+    #CENTRALIZANDO O TEXTO
+    pagina.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    texto_hello =ft.Text(value="Mensagem do Dia",
+                        color ="#A0C6E9",
+                        font_family="italic",
+                        size=60,
+                        weight=ft.FontWeight.BOLD,)
+                        
+    subtitulo =ft.Text(value="Um clique, um destino",
+                           color ="#A0C6E9",
+                           font_family="consolas",
+                           size=40,
+                           weight=ft.FontWeight.BOLD,)
+
+    
+    def mensagem():
+        lista_mensagens = ["Você tentou... e foi isso.",
+                        "Sua ideia saiu pela porta dos fundos.",
+                        "Você tem um talento especial para complicar o simples.",
+                        "Impressionante... mas pelo motivo errado.",
+                        "Até um relógio parado acerta duas vezes.",
+                        "Sua estratégia foi um verdadeiro mistério.",
+                        "Você quase acertou... quase.",
+                        "Parece que a sorte estava de folga.",
+                        "Você consegue errar com confiança.",
+                        "Essa foi uma obra de improviso.",
+                        "Você fez o difícil parecer impossível.",
+                        "Sua organização tirou férias.",
+                        "Foi uma tentativa bem corajosa.",
+                        "Você transformou um detalhe em um problemão.",
+                        "Sua pressa chegou antes da sua ideia.",
+                        "Você tem uma criatividade bem... inesperada.",
+                        "Hoje não era o seu dia.",
+                        "Seu plano precisava de um plano B.",
+                        "Você conseguiu surpreender todo mundo."]
+
+        lista_imagem = ["img/memes/doritos.jpg",
+                        "img/memes/hehehe.png",
+                        "img/memes/woody.jpg",
+                        "img/memes/zoio.jpg",
+                        "img/cachorro.jpg"]
+        
+        sorteio = random.choice(lista_mensagens)
+        sorteio_imagem = random.choice(lista_imagem)
+
+
+        if texto_mensagens.visible ==  True:
+            texto_mensagens.visible =False 
+            pagina.bgcolor = hex(random.randint(0,16700000))
+            texto_mensagens.color = hex(random.randint(0,16700000))
+
+
+
+        else:
+            texto_mensagens.visible =True
+            texto_mensagens.value = sorteio
+
+        if imagem.visible == True:
+            imagem.visible = False
+        else:
+            imagem.visible = True
+            imagem.src = sorteio_imagem
+
+
+    botao = ft.Button(content="Clique Aqui para ver sua mensagem!",
+                     color="#7BAFE0",
+                     on_click=mensagem)
+
+    imagem =ft.Image(src="img/cachorro.jpg",
+                    width=200, 
+                    height=200,
+                    border_radius=20,
+                    visible=False)
+
+
+    texto_mensagens =ft.Text(value="",
+                             color="#0F2763",
+                             size=30,
+                             visible=False)
+
+
+
+
+
+
+
+
+
+
+    pagina.add(texto_hello)
+    pagina.add(subtitulo)
+    pagina.add(botao)
+    pagina.add(texto_mensagens)
+    pagina.add(imagem)
+ft.run(main)
