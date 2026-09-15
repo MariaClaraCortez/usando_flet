e2b226984afa659ea0baca726b8236f7c65317f7
Maria Clara Cortez <maria.c.dias@edu.senai.br>
2026-09-15 08:44:22 -0300

    add comecei a usar o sqlite3 para crir uma tabela e salvar os dados

--- tarefa.py
+++ tarefa.py
@@ -1,5 +1,6 @@
 import flet as ft
 from component_tarefa.classe_campo_tarefa import Campo_Tarefa
+import sqlite3
 
 def main(pagina:ft.Page):
     pagina.title = "Tarefas"
@@ -17,12 +18,35 @@ def main(pagina:ft.Page):
     
     lista_campo_tarefa = []
 
+    # criando a tabela de tarefas no banco de dados SQLITE3
+    conexao = sqlite3.connect("bd_tarefa.sqlite") #conectando ao banco de dados
+    cursor = conexao.cursor() #criando cursor
+    cursor.execute("""
+                   CREATE TABLE IF NOT EXISTS tarefas(
+                   cod_tarefas INTEGER PRIMARY KEY AUTOINCREMENT,
+                   tarefa TEXT,
+                   status TEXT);
+                   """)  #cursor executa esse comando para criar a tabela
+    
+    conexao.commit() #salvando alterações
+    conexao.close() #fechando conexão
+
     def excluir_tarefa(campo_tarefa):
         lista_campo_tarefa.remove(campo_tarefa)
 
     def adicionar_campo_tarefa():
         lista_campo_tarefa.append(Campo_Tarefa(texto_tarefa=campo_tarefa.value,
                                                funcao_excluir=excluir_tarefa))
+
+        conexao = sqlite3.connect("bd_tarefa.sqlite")
+        cursor = conexao.cursor()
+        cursor.execute ("""
+                        INSERT INTO tarefas(tarefa,status)
+                        VALUES (?, ?);
+                           """,
+                             [campo_tarefa.value,"Pendente"])
+        conexao.commit()
+        conexao.close()
         campo_tarefa.value = ""
         
     
